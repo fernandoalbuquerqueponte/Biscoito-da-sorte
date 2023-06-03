7121dc8a3830827e9825941ac0359fc65c6dfbe0
fernando <fernandonetodeveloper@gmail.com>
2023-06-03 20:30:23 -0300

    biscoitodasorteapp

--- App.jsx
+++ App.jsx
@@ -1,8 +1,76 @@
 import React, { useState } from "react"
-import { View, Text, StyleSheet, Button } from "react-native"
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  Image,
+  TouchableOpacity,
+} from "react-native"
 
 export default function App() {
+  const [img, setImg] = useState(require("./src/biscoito.png"))
+
   return (
-    <Text>Olá mundooo</Text>
-  );
-}c
+    <View style={styles.container}>
+      <Image source={img} style={styles.img} />
+
+      <Text style={styles.textofrase}>
+        "Esta é minha primeira frase do biscoito"
+      </Text>
+
+      <TouchableOpacity style={styles.botao} onPress={() => alert("teste")}>
+        <View style={styles.btnArea}>
+          <Text style={styles.btnTexto}>Quebrar Bsicoito</Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.botao, { marginTop: 15, borderColor: "#121212" }]}
+        onPress={() => alert("teste")}
+      >
+        <View style={styles.btnArea}>
+          <Text style={[styles.btnTexto, { color: "#121212" }]}>
+            Reiniciar Biscoito
+          </Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  img: {
+    width: 230,
+    height: 230,
+  },
+  textofrase: {
+    fontSize: 20,
+    color: "#dd7b22",
+    margin: 30,
+    fontStyle: "italic",
+    textAlign: "center",
+  },
+  botao: {
+    width: 230,
+    height: 50,
+    borderColor: "#dd7b22",
+    borderWidth: 2,
+    borderRadius: 25,
+  },
+  btnArea: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  btnTexto: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#dd7b22",
+  },
+})
